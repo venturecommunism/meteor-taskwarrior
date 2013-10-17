eb194ead6fd576fa0f9e606185a9ed45ef47f752
root <root@glibtest.(none)>
2013-10-17 20:35:42 +0000

    removed var from variables in config file

--- server/config.js
+++ server/config.js
@@ -1,12 +1,12 @@
 //replace with your task server's ip
-var taskserver = '127.0.0.1'
+taskserver = '127.0.0.1'
 //replace with your task server's port (default 6544)
-var port = 6544
+port = 6544
 //replace org with your user's organization e.g. Public
-var org = 'Public'
+org = 'Public'
 //replace user with your user e.g. USER
-var user = 'USER'
+user = 'USER'
 //replace key with your user's key e.g. ddea9923-fgg3-3922-c958-23cgdeaa0584
-var key = 'ddea9923-fgg3-3922-c958-23cgdeaa0584'
+key = 'ddea9923-fgg3-3922-c958-23cgdeaa0584'
 
-var myClientPrivateKey = ''
+myClientPrivateKey = ''
