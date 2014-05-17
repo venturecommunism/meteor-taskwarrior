4a7308354b16236fc86f0d9b039897ddcf712975
Venture Communist <me@example.com>
2014-05-17 16:24:31 +0000

    fixing kickstart button and refactoring project_filters code

--- client/main.js
+++ client/main.js
@@ -5,10 +5,10 @@ Meteor.subscribe("tasksbacklog")
 Session.set('adding_newtask', false);
 Session.set('processing_task', false);
 
-Session.setDefault('process_status', true);
+Session.setDefault('process_status', false);
 Session.setDefault('organize_status', false);
 Session.setDefault('review_status', false);
-Session.setDefault('do_status', false);
+Session.setDefault('do_status', true);
 
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_AND_EMAIL'
--- client/organize/organize.js
+++ client/organize/organize.js
@@ -20,47 +20,6 @@ function selectDepProcessing(e,t){
   focusText(t.find(".modal .title"));
 };
 
-// Pick out the unique projects from all tasks in pending tasks
-Template.project_filter.projects = function () {
-  var project_infos = [];
-  var total_count = 0;
-
-//the cursor below doesn't have the formattednow waiting field
-  Taskspending.find({status: {$in: ["waiting", "pending"]}, tags: {$not: "inbox"}}, {sort: {due: -1}}).forEach(function (task) {
-//the bottom may be for arrays of tags rather than the single project that is allowed
-//    _.each(task.project, function (project) {
-      var project_info = _.find(project_infos, function (x) { return x.project === task.project; });
-      if (! project_info)
-        project_infos.push({project: task.project, count: 1});
-      else
-        project_info.count++;
-//    });
-    total_count++;
-  });
-
-  project_infos = _.sortBy(project_infos, function (x) { return x.project; });
-  project_infos.unshift({project: null, count: total_count});
-
-  return project_infos;
-};
-
-Template.project_filter.project_text = function () {
-   return this.project || "All items";
-};
-
-Template.project_filter.events({
-  'mousedown .project': function () {
-    if (Session.equals('project_filter', this.project))
-      Session.set('project_filter', null);
-    else
-      Session.set('project_filter', this.project);
-  }
-});
-
-Template.project_filter.selected = function () {
-  return Session.equals('project_filter', this.project) ? 'active btn-danger' : '';
-};
-
 Template.organize.events({
   'click #btnAddItem': function (e,t){
     Session.set('list_adding',true);  
@@ -95,12 +54,11 @@ Template.organize.events({
         Taskspending.update({_id: this._id},{$set:{project:e.target.value}})
       }
   },
-  'click .kickstart.active': function (e,t) {
+  'click .kickstart.choosekickstart': function (e,t) {
     Taskspending.update({_id: this._id}, {$set:{tags:"mit"}});
     Meteor.flush()
-    console.log('tmp test')
   },
-  'click .kickstart.btn': function (e,t) {
+  'click .kickstart.btn-danger': function (e,t) {
     Taskspending.update({_id: this._id}, {$unset:{tags:"mit"}})
     Meteor.flush()
   },
