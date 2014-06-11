915c3affb129baac403ebdf6bf23a2688ea3d9ba
Venture Communist <me@example.com>
2014-06-11 03:59:23 +0000

    display the date on the Do tab

--- client/do/do.js
+++ client/do/do.js
@@ -53,3 +53,13 @@ else {
 return false
 }
 }
+
+Template.do.date = function () {
+var dt = new Date();
+
+// Display the month, day, and year. getMonth() returns a 0-based number.
+var month = dt.getMonth()+1;
+var day = dt.getDate();
+var year = dt.getFullYear();
+return 'Today is ' + month + '/' + day + '/' + year;
+}
