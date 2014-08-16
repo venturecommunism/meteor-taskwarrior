e00d8b357447e4643250fb8e7e00e3659457e554
Venture Communist <me@example.com>
2014-08-16 23:40:10 +0000

    got rid of somedaymaybe and waiting conditionals

---
+++ client/review/documenteditingdialog/documenteditingdialog.js
@@ -0,0 +1,16 @@
+Template.documenteditingdialog.doctitle = function () {
+  return Taskspending.findOne({_id: Session.get('documentediting')}).description
+}
+
+Template.documenteditingdialog.payload = function () {
+  return Taskspending.findOne({_id: Session.get('documentediting')}).payload
+}
+
+Template.documenteditingdialog.events({
+  'click .modal .savebutton': function (e,t) {
+    Taskspending.update({_id: Session.get('documentediting')}, {$set: {payload: t.findAll('.modal textarea')[0].value}})
+  },
+  'click .doceditclose': function (e,t) {
+    Session.set('documentediting',false)
+  }
+});
