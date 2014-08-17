dcf14bdf959e584b7e58c4a5fd724817f1c2ea4a
Venture Communist <me@example.com>
2014-08-17 00:55:13 +0000

    basic functionality for multicontext do tab

--- client/do/multicontext.js
+++ client/do/multicontext.js
@@ -1,3 +1,17 @@
 Template.multicontext.multicontext = function () {
-  Session.get("multicontext")
+  var multicontext = Session.get("multicontext")
+  return multicontext
 }
+
+Template.multicontext.multitasks = function () {
+  if (!Session.get('do_context')){
+    return Taskspending.find({status: {$in: ["waiting", "pending"]}, $and: [{tags: {$ne: "inbox"}}, {project: {$exists: false}}, {context: this.toString()}]}, {sort: {due:1}})
+  }
+}
+
+Template.multicontext.multitasks2 = function () {
+  if (!Session.get('do_context')){
+    return Taskspending.find({status: {$in: ["waiting", "pending"]}, $and: [{tags: {$not: "inbox"}}, {tags: "mit"}, {context: this.toString()}]}, {sort: {due:1}})
+  }
+}
+
