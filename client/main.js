e516c7ae5ab97e1997949523304b522e57e2c615
Venture Communist <me@example.com>
2014-08-29 11:16:02 +0000

    basic timers working

--- client/main.js
+++ client/main.js
@@ -8,9 +8,27 @@ cursor = Taskspending.find({status: {$in: ["waiting", "pending"]}, $and: [{tags:
 cursor.forEach(function (entry) {
   var clock, interval, timeLeft;
 
+var formattednow = formattedNow()
+var newstringparts = entry.due.substring(0,4) + "-" + entry.due.substring(4,6) + "-" + entry.due.substring(6,8) + "-" + entry.due.substring(9,11) + "-" + entry.due.substring(11,13) + "-" + entry.due.substring(13,15)
+var newformattednow = formattednow.substring(0,4) + "-" + formattednow.substring(4,6) + "-" + formattednow.substring(6,8) + "-" + formattednow.substring(9,11) + "-" + formattednow.substring(11,13) + " " + formattednow.substring(13,15)
+console.log('duedate')
+console.log(newstringparts)
+console.log('newformattednow')
+console.log(newformattednow)
+var momentone = moment(newstringparts, "YYYY-MM-DD-HH-mm-ss")
+var momenttwo = moment(newformattednow, "YYYY-MM-DD-HH-mm-ss")
+console.log(momentone)
+console.log(momenttwo)
+var diff = momentone.diff(momenttwo, 'seconds')
+console.log('grand total is')
+console.log(diff)
+
+
+
+
 console.log('entry is ' + entry.due)
 console.log(moment())
-  clock = 10;
+  clock = diff;
 
   var uuid = entry.uuid
 
