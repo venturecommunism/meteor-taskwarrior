55458845badede5c808707592418953a390bab4b
Venture Communist <me@example.com>
2015-02-26 13:16:09 +0000

    basic past and upcoming calendar items

--- client/review/reviewdialog5/reviewdialog5.js
+++ client/review/reviewdialog5/reviewdialog5.js
@@ -11,3 +11,8 @@ Template.reviewdialog5.events({
     Session.set('review_dialog_5',false)
   }
 })
+
+Template.reviewdialog5.pastcalendar = function () {
+  return Tasksbacklog.find({status: {$in: ["completed"]}, $and: [{tags: {$ne: "inbox"}}, {project: {$exists: false}},{due: {$exists: true}}, {context: {$exists: false}}]}, {sort: {due:-1}})
+  return Taskspending.find({status: {$in: ["completed", "waiting", "pending"]}, $and: [{tags: {$ne: "inbox"}}, {project: {$exists: false}}, {context: {$exists: false}}]}, {sort: {due:1}})
+}
--- client/review/reviewdialog6/reviewdialog6.js
+++ client/review/reviewdialog6/reviewdialog6.js
@@ -11,3 +11,9 @@ Template.reviewdialog6.events({
     Session.set('review_dialog_6',false)
   }
 })
+
+Template.reviewdialog6.upcomingcalendar = function () {
+  return Taskspending.find({status: {$in: ["waiting", "pending"]}, $and: [{tags: {$ne: "inbox"}}, {project: {$exists: false}},{due: {$exists: true}}, {context: {$exists: false}}]}, {sort: {due:1}})
+  return Taskspending.find({status: {$in: ["completed", "waiting", "pending"]}, $and: [{tags: {$ne: "inbox"}}, {project: {$exists: false}}, {context: {$exists: false}}]}, {sort: {due:1}})
+}
+
