6ff5939212e1193422a42c12d3222e5358fb1a2f
Venture Communist <me@example.com>
2015-02-27 16:53:30 +0000

    changed back to curl

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-apt-get update && apt-get install -y git
-wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | /bin/bash
+sudo apt-get update && sudo apt-get install -y git curl
+curl https://raw.github.com/creationix/nvm/master/install.sh | /bin/bash
 [[ -s ~/.nvm/nvm.sh ]] && . ~/.nvm/nvm.sh
 nvm install 0.10
 nvm alias default 0.10
 nvm use 0.10
-wget -q0- https://install.meteor.com | /bin/sh
+curl https://install.meteor.com | /bin/sh
 git clone https://github.com/venturecommunism/meteor-taskwarrior.git
