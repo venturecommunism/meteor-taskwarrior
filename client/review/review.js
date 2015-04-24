a7f9e143d4ea1f4daec862d2686c75eaaa3a8662
Venture Communist <me@example.com>
2015-04-24 15:49:42 +0000

    sortable MIT list

--- client/review/review.js
+++ client/review/review.js
@@ -54,13 +54,11 @@ Template.process.events({
 });
 
 Template.review.kickstartertask = function () {
-  console.log('walawala')
-  console.log(Taskspending.findOne({tags: "kickstart", project: this.project}))
   return Taskspending.find({tags: "kickstart", project: this.project})
 }
 
 Template.review.mits = function () {
-  return Taskspending.find({tags: "mit"})
+  return Taskspending.find({tags: "mit"}, {sort: {rank: 1}})
 }
 
 Template.review.events({
@@ -213,8 +211,43 @@ Template.review.rendered = function () {
     Session.get('helpsesh')
     $('.active-project.nokickstarttask').detach().prependTo('ul#project_list')
     $('.active-project.kickstarttask').detach().appendTo('ul#project_list')
-  }
-)
+
+
+//jquery sortable code, using sortable meteor package
+   this.$('.mit_task_list').sortable({
+        stop: function(e, ui) {
+          // get the dragged html element and the one before
+          //   and after it
+          el = ui.item.get(0)
+          before = ui.item.prev().get(0)
+          after = ui.item.next().get(0)
+ 
+          // Here is the part that blew my mind!
+          //  Blaze.getData takes as a parameter an html element
+          //    and will return the data context that was bound when
+          //    that html element was rendered!
+          if(!before) {
+            //if it was dragged into the first position grab the
+            // next element's data context and subtract one from the rank
+            newRank = Blaze.getData(after).rank - 1
+          } else if(!after) {
+            //if it was dragged into the last position grab the
+            //  previous element's data context and add one to the rank
+            newRank = Blaze.getData(before).rank + 1
+          }
+          else
+            //else take the average of the two ranks of the previous
+            // and next elements
+            newRank = (Blaze.getData(after).rank +
+                       Blaze.getData(before).rank)/2
+ 
+          //update the dragged Item's rank
+console.log(newRank)
+          Taskspending.update({_id: Blaze.getData(el)._id}, {$set: {rank: newRank}})
+        }
+    })
+// end of sortable code
+})
 }
 
 Template.review.projopen = function () {
