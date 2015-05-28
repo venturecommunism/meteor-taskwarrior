f30ad2e5a8d44eea587b0192a57e5a536cb48da8
Venture Communist <me@example.com>
2015-05-28 23:25:15 +0000

    putting date on Review tab

--- client/review/review.js
+++ client/review/review.js
@@ -50,6 +50,15 @@ Template.review.helpers({
       return ''
     }
   },
+  date: function () {
+    var dt = new Date();
+    var month = dt.getMonth()+1;
+    var day = dt.getDate();
+    var year = dt.getFullYear();
+    var time = dt.getTime();
+    var date = new Date(time);
+    return 'Date and time is ' + date.toString();
+  },
 })
 
 Template.documenteditingdialog.helpers({
