14fd9bc19b0342e36f44b9e7c8dde602877ab8a0
Venture Communist <me@example.com>
2015-05-31 15:06:10 +0000

    opening and closing projects works again

--- client/review/review.js
+++ client/review/review.js
@@ -186,24 +186,6 @@ Session.set('helpsesh',false)
       Taskspending.update({_id: taskid}, {$push: {tags: "somedaymaybeproj"}})
     }
   },
-  'click .reviewproject': function (e,t) {
-    if (Session.equals('projopen', this.project)) {
-    if (!$('.active-project.nokickstarttask')) {
-      Session.set('projopen',false)
-    } else {
-      var nokickstartproj = Taskspending.findOne({_id: $('.active-project.nokickstarttask .task_item button').last().attr('id')})
-      if (nokickstartproj) {
-        Session.set('projopen', nokickstartproj.project)
-      } else {
-        Session.set('projopen', false)
-      }
-    }
-    $('.active-project.nokickstarttask').detach().prependTo('ul#project_list')
-    $('.active-project.kickstarttask').detach().appendTo('ul#project_list')
-    } else {
-      Session.set('projopen', this.project);
-    }
-  },
   'click .startprocessing-button': selectTaskProcessing,
 });
 
