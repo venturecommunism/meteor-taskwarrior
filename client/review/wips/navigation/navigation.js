24498d8821bd4827d36108f948fd092c51dbb25d
Venture Communist <me@example.com>
2015-07-10 13:05:17 +0000

    getting closer but orgtasks doesn't work for navigation items in the sortable WIP whiteboard

---
+++ client/review/wips/navigation/navigation.js
@@ -0,0 +1,71 @@
+// begin modular subscription loading
+
+Template.navigation.created = function () {
+
+  // 1. Initialization
+
+  var instance = this;
+
+  // initialize the reactive variables
+  instance.loaded = new ReactiveVar(0);
+  instance.navigationlimit = new ReactiveVar(5);
+
+  // 2. Autorun
+
+  // will re-run when the "limit" reactive variables changes
+  this.autorun(function () {
+
+    // get the limit
+    var navigationlimit = instance.navigationlimit.get();
+
+    // console.log("Asking for "+navigationlimit+" navigation tasks…")
+
+    // subscribe to the posts publication
+    var subscription = instance.subscribe('taskspendingnavigation', navigationlimit)
+
+    // if subscription is ready, set limit to newLimit
+    if (subscription.ready()) {
+      // console.log("> Received "+navigationlimit+" navigation tasks. \n\n")
+      instance.loaded.set(navigationlimit);
+    } else {
+      // console.log("> Subscription is not ready yet. \n\n");
+    }
+  });
+
+  // 3. Cursor
+
+  instance.taskspendingnavigation = function() {
+    return Taskspending.find({context: "navigation", tags: {$ne: "largercontext"}}, {sort: {rank: 1}, limit: instance.loaded.get()})
+  }
+
+};
+
+Template.navigation.helpers({
+  // the posts cursor
+  navigation: function () {
+    return Template.instance().taskspendingnavigation();
+  },
+  // are there more posts to show?
+  hasMorePosts: function () {
+    return Template.instance().taskspendingnavigation().count() >= Template.instance().navigationlimit.get();
+  }
+});
+
+Template.navigation.events({
+  'click .load-more-navigation': function (event, instance) {
+    event.preventDefault();
+
+    // get current value for limit, i.e. how many posts are currently displayed
+    var navigationlimit = instance.navigationlimit.get();
+
+    // increase limit by 5 and update it
+    navigationlimit += 5;
+    instance.navigationlimit.set(navigationlimit)
+  },
+  'click .closenavigationsection': function(e,t){
+    Session.set('navigationhidden', true)
+  },
+});
+
+// end modular subscription loading
+
---
+++ client/review/wips/navigation/navtask/navtask.js
@@ -0,0 +1,97 @@
+Session.set('editing_itemname', null);
+
+Template.navtask.helpers({
+  editing: function () {
+    return Session.equals('editing_itemname', this._id);
+  },
+  is_kickstarter: function () {
+    var truefalse = null
+    if (Taskspending.findOne({_id: this._id, tags: "kickstart"})) {
+      truefalse = true
+    }
+    return truefalse
+  },
+  nokickstart: function () {
+    return !Taskspending.findOne({_id: this._id, tags: "kickstart"});
+  },
+  mitornot: function () {
+    if (Taskspending.findOne({_id: this._id, tags: "mit"})) {
+      return 'active'
+    }
+    else {
+      return ''
+    }
+  },
+})
+
+Template.navtask.events({
+  'click .choosesequential': function (e, t) {
+    if (Taskspending.findOne({_id: this._id, tags: "milestone"})) {
+      Taskspending.update({_id: this._id}, {$pull: {tags: "milestone"}})
+    }
+    else {
+      Taskspending.update({_id: this._id}, {$push: {tags: "milestone"}})
+    }
+  },
+  'dblclick .todo-item': function (e, t) {
+//    alert('Hi');
+    Session.set('editing_itemname', this._id);
+    Meteor.flush(); // update DOM before focus
+    focus_field_by_id("todo-input");
+  },
+  'focusout #todo-input': function (e, t) {
+    Session.set('editing_itemname', null);
+    Meteor.flush();
+  },
+  'keyup #todo-input': function (e,t) {
+    if (e.which === 13)
+    {
+      var taskVal = String(e.target.value || "");
+      if (taskVal)
+      {
+        var formattednow = formattedNow()
+        var uuid = this.uuid
+console.log(uuid)
+        console.log(Tasksbacklog.insert({description: taskVal, entry: formattednow, uuid:uuid}))
+        console.log(Taskspending.update({_id:this._id},{$set:{description: taskVal, entry: formattednow}}))
+        Session.set('editing_itemname', null);
+       }
+     }
+  },
+  'click .mit': function (e,t) {
+    if (Taskspending.findOne({_id: this._id, tags: "mit"})) {
+      Taskspending.update({_id: this._id}, {$pull: {tags: "mit"}})
+      Taskspending.update({_id: this._id}, {$pull: {wip: "contwip"}})
+      if (!Taskspending.findOne({project: this.project, tags: "mit"})) {
+        var projectid = Taskspending.findOne({project: this.project, tags: "largeroutcome"})._id
+        Taskspending.update({_id: projectid}, {$push: {tags: "kickstarterless"}})
+      }
+    }
+    else {
+      Taskspending.update({_id: this._id}, {$push: {tags: "mit"}})
+      Taskspending.update({_id: this._id}, {$push: {wip: "contwip"}})
+      Taskspending.update({_id: projectid}, {$pull: {tags: "kickstarterless"}})
+      if (!this.rank && this.project && Taskspending.findOne({project: this.project, tags: "largeroutcome"}).rank) {
+        var rank = Taskspending.findOne({project: this.project, tags: "largeroutcome"}).rank
+        Taskspending.update({_id: this._id}, {$set: {rank: rank}})
+      }
+    }
+  },
+  'click .choosechecklistitem': function (e,t) {
+    if (Taskspending.findOne({_id: this._id, tags: "mit"})) {
+      Taskspending.update({_id: this._id}, {$pull: {tags: "checklistitem"}})
+    }
+    else {
+      Taskspending.update({_id: this._id}, {$push: {tags: "checklistitem"}})
+    }
+  }
+});
+
+// Finds a text input in the DOM by id and focuses it.
+var focus_field_by_id = function (id) {
+  var input = document.getElementById(id);
+  if (input) {
+    input.focus();
+    input.select();
+  }
+}
