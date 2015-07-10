bd677ce6621e38a8d70abb4fb4d0abb4b6db5ba5
Venture Communist <me@example.com>
2015-07-10 07:55:18 +0000

    changing css on the map, was also testing to see if rendering the map based on user location could work the first time

--- client/gmaps.js
+++ client/gmaps.js
@@ -1,5 +1,7 @@
 Meteor.subscribe('Coords');
 
+Session.setDefault('maphidden', false)
+
 var map;
 var directionsDisplay;
 var directionsService;
@@ -8,10 +10,51 @@ var pointsArr = new Array();
 Template.mapview.helpers({
   counter: function () {
     return Coords.find().count();
-  }
+  },
+  maphidden: function () {
+    return Session.equals("maphidden", true)
+  },
+
 });
 
 Template.mapview.events = {
+  'click .closemapsection': function(e,t) {
+    Session.set('maphidden', true)
+  },
+  'click .openmapsection': function(e,t) {
+    Session.set('maphidden', false)
+    var latlng = Geolocation.latLng()
+setTimeout(function() {
+      // Create map
+  mapOptions = {
+    zoom: 16,
+    center: latlng
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+  directionsService = new google.maps.DirectionsService();
+
+  // Add listener
+  google.maps.event.addListener(map, 'click', addLatLng);
+
+  directionsDisplay = new google.maps.DirectionsRenderer();
+
+  $('#directions-panel').empty()
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('directions-panel'));
+
+  function addLatLng(event) {
+
+    // Add coordinates into db
+    var point = {
+      'lat': event.latLng.lat(),
+      'lng': event.latLng.lng()
+    };
+    Coords.insert(point);
+  }
+}, 1 * 1000);
+  },
   'click #clearButton': function () {
     Meteor.call('removeAllCoords');
       // Create map
@@ -49,6 +92,7 @@ Template.mapview.events = {
 
 Template.mapview.rendered = function () {
   // Create map
+if (Session.equals('maphidden', false)) {
   var mapOptions = {
     zoom: 16,
     center: new google.maps.LatLng(40.4378271, -3.6795367)
@@ -75,6 +119,7 @@ Template.mapview.rendered = function () {
     };
     Coords.insert(point);
   }
+} // Session map hidden false
 };
 
 function drawPath() {
