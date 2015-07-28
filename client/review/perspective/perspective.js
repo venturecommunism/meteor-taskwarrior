d635614bbd22803dc6e2a04f8723d7a15edf8305
Venture Communist <me@example.com>
2015-07-28 16:19:31 +0000

    areas of responsibility

--- client/review/perspective/perspective.js
+++ client/review/perspective/perspective.js
@@ -2,4 +2,43 @@ Template.perspective.events({
   'click .closeperspectivesection': function(e,t){
     Session.set('perspectivehidden', true)
   },
+  'click .newaor.btn': function(e,t){
+    Session.set('editingaor', true)
+    Meteor.flush()
+    focusText(t.find("#add-newaor"))
+  },
+  'focusout #add-newaor': function(e,t){
+    Session.set('editingaor', false)
+  },
+  'keyup #add-newaor': function (e,t) {
+    if (e.which === 13)
+    {
+      var aorVal = String(e.target.value || "");
+      if (aorVal)
+      {
+        var formattednow = formattedNow()
+        if (Taskspending.findOne({rank: {$exists: 1}}, {sort: {rank: 1}})) {
+          var rank = Taskspending.findOne({rank: {$exists: 1}}, {sort: {rank: 1}}).rank - 1
+        }
+        else {
+          var rank = 0
+        }
+        Tasksbacklog.insert({description: e.target.value, owner: Meteor.userId(), entry: formattednow, tags: ['largeroutcome', 'aor'], rank: rank})
+        Taskspending.insert({description: e.target.value, owner: Meteor.userId(), entry: formattednow, tags: ['largeroutcome', 'aor'], rank: rank})
+        Session.set('editingaor', false);
+       }
+     }
+  },
+})
+
+Template.perspective.helpers({
+  projects: function () {
+    return Taskspending.find({$and: [{tags: "largeroutcome"}, {tags: {$ne: "aor"}}]}, {sort: {rank: 1}})
+  },
+  aor: function() {
+    return Taskspending.find({tags: "aor"})
+  },
+  new_aor: function() {
+    return Session.equals('editingaor', true)
+  },
 })
