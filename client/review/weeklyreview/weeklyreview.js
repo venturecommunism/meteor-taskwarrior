d124dc3ab90fea8d447888da4842bbd980e03a16
Venture Communist <me@example.com>
2015-08-10 18:51:10 +0000

    opens sections for weekly review

--- client/review/weeklyreview/weeklyreview.js
+++ client/review/weeklyreview/weeklyreview.js
@@ -22,7 +22,14 @@ Template.weeklyreview.events({
     Taskspending.update({_id: this._id}, {$push: {tags: "aorfocus"}})
     Taskspending.find({tags: "aorfocus", _id: {$ne: this._id}}).forEach( function (doc) {
       Taskspending.update({_id: doc._id}, {$pull: {tags: "aorfocus"}})
-    })    
+    })
+    Session.set("wipshidden", false)
+    Session.set("mitshidden", false)
+    Session.set("nextactionshidden", false)
+    Session.set("projectshidden", false)
+    Session.set("readandreviewhidden", false)
+    Session.set("waitingforshidden", false)
+    Session.set("projectlesssomedaymaybeshidden", false)    
   },
   'change [type=checkbox]': function(e,t) {
     if (this._id && Taskspending.findOne({_id: this._id, tags: "aor"})) {
