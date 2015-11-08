991a9758af0c0343de435c4ecb14029836537f59
Venture Communist <me@example.com>
2015-11-08 15:33:39 +0000

    made the date into a realtime variable

--- client/date/date.js
+++ client/date/date.js
@@ -1,11 +1,13 @@
 Template.date.helpers({
   date: function () {
-    var dt = new Date();
-    var month = dt.getMonth()+1;
-    var day = dt.getDate();
-    var year = dt.getFullYear();
-    var time = dt.getTime();
-    var date = new Date(time);
-    return 'Date and time is ' + date.toString();
+    return 'Date and time is ' + Session.get('now')
   },
+})
+
+Meteor.startup(function() {
+  Session.set('now', new Date())
+
+  Meteor.setInterval(function() {
+    Session.set('now', new Date())
+  }, 500)
 })
