e8ffabd92ba817ddf9fb65e80f62c9f679754caa
Venture Communist <me@example.com>
2015-11-09 19:34:46 +0000

    storing 'now' as a session variable

--- client/date/date.js
+++ client/date/date.js
@@ -1,13 +1,13 @@
 Template.date.helpers({
   date: function () {
-    return 'Date and time is ' + Session.get('now')
+    return 'Date and time is ' + timestamptomoment(Session.get('now')).format()
   },
 })
 
 Meteor.startup(function() {
-  Session.set('now', new Date())
+  Session.set('now', formattedNow())
 
   Meteor.setInterval(function() {
-    Session.set('now', new Date())
+    Session.set('now', formattedNow())
   }, 500)
 })
