18111a488aa12a5e05f9feeffdce2b3d56ab7a97
Venture Communist <me@example.com>
2016-01-24 20:02:30 +0000

    archive and trash

---
+++ client/common/aortimeviewtaskinbox_item/nonactionable/nonactionable.js
@@ -0,0 +1,40 @@
+Template.nonactionable.helpers({
+  'click .trash': function() {
+    trashtask = Taskspending.findOne({_id: this._id})
+    trashtask.status = 'completed'
+    var i = trashtask.tags.indexOf("inbox")
+    if(i != -1) {
+      trashtask.tags.splice(i, 1)
+    }
+    var i = trashtask.tags.indexOf("aorinbox")
+    if(i != -1) {
+      trashtask.tags.splice(i, 1)
+    }
+    if (trashtask.tags.length == 0) {
+      delete trashtask.tags
+    }
+    id = trashtask._id
+    delete trashtask._id
+    Tasksbacklog.insert(trashtask)
+    Taskspending.remove(trashtask)
+  },
+  'click .archive': function () {
+    archivetask = Taskspending.findOne({_id: this._id})
+    var i = somedaymaybetask.tags.indexOf("inbox")
+    if(i != -1) {
+      archivetask.tags.splice(i, 1)
+    }
+    var i = archivetask.tags.indexOf("aorinbox")
+    if(i != -1) {
+      archivetask.tags.splice(i, 1)
+    }
+    archivetask.tags.push("movetoarchive")
+    id = archivetask._id
+    delete archivetask._id
+    Tasksbacklog.insert(archivetask)
+    Taskspending.update({_id: id},{$set: archivetask})
+    Taskspending.update({_id: id},{$pull: {tags: "inbox"}})
+    Taskspending.update({_id: id},{$pull: {tags: "aorinbox"}})
+    Taskspending.update({_id: id},{$set: {context: "movetoarchive"}})
+  },
+})
